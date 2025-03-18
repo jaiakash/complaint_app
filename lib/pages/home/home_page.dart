a6953c929317c4a4593b8fd7fa7e583c249755cd
Ayush Yadav <129136683+0102ayush@users.noreply.github.com>
2025-03-18 15:08:10 +0530

    Feature/phone authentication (#17)
    * feat(auth): Add Google Sign-In client ID meta tag for web authentication
    
    * Fixes: Phone-auth
    
    * fixes: autofill using Google's SMS Retriever API
    
    * fixes Home screen after login!

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:complaint_app/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = AuthService().currentUser;
     final AuthService _authService = AuthService();
+
+    // Get user details
+    String displayName = user?.displayName ?? "No username";
+    String email = user?.email ?? "No email";
+    String phoneNumber = user?.phoneNumber ?? "No phone number";
 
     return Scaffold(
       appBar: AppBar(
@@ -31,14 +37,18 @@ class HomeScreen extends StatelessWidget {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            Text(
-              'User: ${user?.displayName ?? 'No username'}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Email: ${user?.email ?? 'No email'}',
-              style: const TextStyle(fontSize: 16),
-            ),
+            // Show email & name for Google users, otherwise show phone number
+            if (user != null) ...[
+              if (user.email != null && user.email!.isNotEmpty)
+                Text('Username: $displayName', style: const TextStyle(fontSize: 16)),
+
+              if (user.displayName != null && user.displayName!.isNotEmpty)
+                Text('Email: $email', style: const TextStyle(fontSize: 16)),
+
+              if (user.phoneNumber != null && user.phoneNumber!.isNotEmpty)
+                Text('Phone: $phoneNumber', style: const TextStyle(fontSize: 16)),
+            ] else
+              const Text('No user logged in', style: TextStyle(fontSize: 16)),
           ],
         ),
       ),
